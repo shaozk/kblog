142b8178c2b89433d5b02ab679bb52151c1a9d60
shaozk <913667678@qq.com>
2021-01-06 21:21:51 +0800

    统一返回结果

---
+++ src/main/java/org/example/blog/response/ResponseResult.java
@@ -0,0 +1,40 @@
+package org.example.blog.response;
+
+public class ResponseResult {
+    private boolean success;
+    private int code;
+    private String message;
+    private Object data;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getDate() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
